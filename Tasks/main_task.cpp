d263e5b237a988d782d1829a9a3b28d844ef931a
rogue-wave <zhangjingjie@zju.edu.cn>
2025-11-20 19:42:26 +0800

    <feat>chu_shi_hua

---
+++ Tasks/main_task.cpp
@@ -0,0 +1,104 @@
+/**
+*******************************************************************************
+* @file      :main_task.cpp
+* @brief     :
+* @history   :
+*  Version     Date            Author          Note
+*  V0.9.0      yyyy-mm-dd      <author>        1. <note>
+*******************************************************************************
+* @attention :
+*******************************************************************************
+*  Copyright (c) 2024 Hello World Team，Zhejiang University.
+*  All Rights Reserved.
+*******************************************************************************
+*/
+
+/* Includes ------------------------------------------------------------------*/
+#include "main_task.hpp"
+#include "system_user.hpp"
+
+#include "DT7.hpp"
+#include "HW_can.hpp"
+#include "dm4310_drv.hpp"
+#include "iwdg.h"
+#include "math.h"
+#include "imu_task.hpp"
+/* Private macro -------------------------------------------------------------*/
+/* Private constants ---------------------------------------------------------*/
+/* Private types -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+/* External variables --------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+//常量定义区
+//变量定义区
+float rc_rv__= 0;
+float rc_rh__ = 0;
+uint32_t tick = 0;
+//函数声明区
+void MODE1(void);
+void set_tor(float tor);
+
+
+namespace remote_control = hello_world::devices::remote_control;
+static const uint8_t kRxBufLen = remote_control::kRcRxDataLen;
+static uint8_t rx_buf[kRxBufLen];
+remote_control::DT7 *rc_ptr;
+
+void RobotInit(void) { rc_ptr = new remote_control::DT7(); ImuInit(); }
+
+void MainInit(void) {
+  RobotInit();
+
+  // 开启CAN1和CAN2
+  CanFilter_Init(&hcan1);
+  HAL_CAN_Start(&hcan1);
+  HAL_CAN_ActivateNotification(&hcan1, CAN_IT_RX_FIFO0_MSG_PENDING);
+
+  CanFilter_Init(&hcan2);
+  HAL_CAN_Start(&hcan2);
+  HAL_CAN_ActivateNotification(&hcan2, CAN_IT_RX_FIFO1_MSG_PENDING);
+
+  // 开启遥控器接收
+  HAL_UARTEx_ReceiveToIdle_DMA(&huart3, rx_buf, kRxBufLen);
+
+  // 开启定时器
+  HAL_TIM_Base_Start_IT(&htim6);
+}
+
+void MainTask(void) {
+   tick++;
+   ImuUpdate();
+   if(tick<1000)
+   {
+    set_tor(0.0f);
+    return;
+   }
+
+  }
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
+
+  if (htim == &htim6) {
+    MainTask();
+  }
+}
+uint8_t rx_data = 0;
+void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size) {
+  if (huart == &huart3) {
+    if (Size == remote_control::kRcRxDataLen) {
+      // TODO:在这里刷新看门狗
+      rc_ptr->decode(rx_buf);
+    }
+
+    HAL_UARTEx_ReceiveToIdle_DMA(&huart3, rx_buf, kRxBufLen);
+  }
+}
+void set_tor(float tor)
+{
+  enable_motor_mode(&hcan2,1,MIT_MODE);
+  mit_ctrl(&hcan2,1,0,0,0,0,tor);
+}
+void MODE1(void)
+{
+
+}
